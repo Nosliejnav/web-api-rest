cdd17beeaf5839fb6ecdb45913fc8e3a76865a05
Nosliejnav <vanjeilsonm10@gmail.com>
2025-12-08 17:55:45 -0300

    first commit

---
+++ src/main/java/com/example/web_api_rest/WebApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/web_api_rest/WebApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
