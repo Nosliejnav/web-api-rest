531bdb00a71786b6856d368d618e6cd1d0a1ea85
Nosliejnav <vanjeilsonm10@gmail.com>
2025-12-09 17:20:25 -0300

    RestController Add @DeleteMapping deleteById

--- src/main/java/com/example/web/api/rest/controller/UsuarioController.java
+++ src/main/java/com/example/web/api/rest/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.example.web.api.rest.controller;
 import com.example.web.api.rest.model.Usuario;
 import com.example.web.api.rest.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,11 @@ public class UsuarioController {
     @GetMapping("/users/{username}")
     public Usuario getOne(@PathVariable("username") String username){
         return repository.findByUsername(username);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void delete(@PathVariable("id")Integer id){
+        repository.deleteById(id);
     }
 
 }
